b845ddb8713c4b27ee417ffbb28d7a3ff9d283a7
quangbuitrong <trongquang190897@gmail.com>
2024-01-17 17:14:17 +0700

    week2: add user management

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -27,16 +27,44 @@ def contact(request):
     return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('psw')
+        if username and password:
+            # check auth
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                # login if valid
+                login(request, user)
+                return redirect('djangoapp:index')
+        context['message'] = "Invalid username or password."
+    return render(request, 'djangoapp/index.html', context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', {})
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('psw')
+        first_name = request.POST.get('firstname')
+        last_name = request.POST.get('lastname')
+
+        if User.objects.filter(username=username).exists():
+            context = {'message': 'User already exists.'}
+            return render(request, 'djangoapp/registration.html', context)
+
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password)
+        login(request, user)
+        return redirect("djangoapp:index")
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
